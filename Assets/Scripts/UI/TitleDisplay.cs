66acc7a1f531a09b0d0c57104bd95ce456fcab75
Mikuto <134473854+Mikuto-Takechi@users.noreply.github.com>
2023-10-19 23:29:14 +0900

    [add]VContainerを追加した。

---
+++ Assets/Scripts/UI/TitleDisplay.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleDisplay : MonoBehaviour
+{
+    [SerializeField] Button _startButton;
+    [SerializeField] Button _optionButton;
+    [SerializeField] Button _licenseButton;
+}
